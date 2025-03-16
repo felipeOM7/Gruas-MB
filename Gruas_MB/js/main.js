ce4fc8b59f42af18707e510dc5b88ffd99561f42
Felipe <felipeosoriom2603@gmail.com>
2025-03-16 03:10:31 -0600

    first commit

---
+++ Gruas_MB/js/main.js
@@ -0,0 +1,124 @@
+(function ($) {
+    "use strict";
+
+    // Dropdown on mouse hover
+    $(document).ready(function () {
+        function toggleNavbarMethod() {
+            if ($(window).width() > 992) {
+                $('.navbar .dropdown').on('mouseover', function () {
+                    $('.dropdown-toggle', this).trigger('click');
+                }).on('mouseout', function () {
+                    $('.dropdown-toggle', this).trigger('click').blur();
+                });
+            } else {
+                $('.navbar .dropdown').off('mouseover').off('mouseout');
+            }
+        }
+        toggleNavbarMethod();
+        $(window).resize(toggleNavbarMethod);
+    });
+
+
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+
+    // Modal Video
+    var $videoSrc;
+    $('.btn-play').click(function () {
+        $videoSrc = $(this).data("src");
+        console.log($videoSrc); // Verifica que la ruta esté correcta
+    });
+
+    $('#videoModal').on('shown.bs.modal', function () {
+        $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+    });
+
+    $('#videoModal').on('hide.bs.modal', function () {
+        $("#video").attr('src', $videoSrc);
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        center: true,
+        autoplay: true,
+        smartSpeed: 1500,
+        margin: 30,
+        dots: true,
+        loop: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            576: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 3
+            }
+        }
+    });
+
+})(jQuery);
+
+
+// Escuchar el evento de clic en el botón de búsqueda
+document.getElementById('searchButton').addEventListener('click', function () {
+    var searchQuery = document.getElementById('searchInput').value.trim().toLowerCase(); // Obtener el valor del input
+
+    // Intentar encontrar la sección correspondiente
+    var section = document.getElementById(searchQuery);
+
+    if (section) {
+        // Si la sección existe, desplazarse a ella
+        section.scrollIntoView({ behavior: 'smooth' });
+    } else {
+        // Si no existe, mostrar una ventana emergente
+        Swal.fire({
+            icon: 'error',
+            title: 'Sección no encontrada',
+            text: 'No hemos encontrado ninguna sección con ese nombre.',
+            confirmButtonText: 'Aceptar'
+        });
+    }
+});
+
+// Opcionalmente, permitir que el usuario presione Enter para buscar
+document.getElementById('searchInput').addEventListener('keypress', function (event) {
+    if (event.key === 'Enter') {
+        document.getElementById('searchButton').click();
+    }
+});
+
+
+document.querySelectorAll('.flip-btn').forEach(button => {
+    button.addEventListener('click', function () {
+        const card = this.closest('.card');
+        card.classList.toggle('flipped');
+    });
+});
+
+
+
+
